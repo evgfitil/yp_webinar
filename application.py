c11e237662f86825d8b415e990627247236401f5
Евгений Ахметзянов <eugenueakhmetzyanov@evgakhm-mbp.dft>
2020-10-28 18:36:59 +0300

    Fix typo

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-    return '<h1 style='color:blue'>Здесь будет приложение</h1>
+    return "<h1 style='color:blue'>Здесь будет приложение</h1>"
 
 
 if __name__ == '__main__':
